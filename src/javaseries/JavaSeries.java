f7eab57ceea2bbb42be209b9e6adc9ff914980bf
muhammadtahaazam@gmail.com <muhammadtahaazam@gmail.com>
2020-07-23 00:45:01 +0500

    Java Course

---
+++ src/javaseries/JavaSeries.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaseries;
+
+/**
+ *
+ * @author Aksystems
+ */
+public class JavaSeries {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Find the surface area of the sphere if the radius is  2cm
+
+//        double surfacearea;
+//        double pi=3.142;
+//        double radius=2;
+//        
+//        surfacearea = 4* pi*radius*radius;
+//        
+//        System.out.println("The surface area of sphere is "+surfacearea);
+
+
+    //Find the volume of  a rectangular box whose dimensions are:Length=2cm width = 1.5cm Height = 1cm 
+    
+        
+    double volume;
+    
+    double length=2;
+    double width=1.5;
+    double heigth=1;
+    
+    
+    volume = length *width*heigth;
+    
+        System.out.println("The volume of rectangle is  "+volume);
+    
+        
+        
+        
+    }
+    
+}
